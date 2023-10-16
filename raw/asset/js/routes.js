f38b408ecde8cdab3da10912be87d44bcec1bf43
Dap Moderator <123246207+thebanon@users.noreply.github.com>
2023-10-15 20:09:17 -0400

    Update WebTools Sources Panel

--- raw/asset/js/routes.js
+++ raw/asset/js/routes.js
@@ -456,86 +456,11 @@ window.routes = function(uri, options) {
                                             }) : null;
                                         }
                                     })
-                                }
-
-                                var filetrees = component.querySelector("#file-trees");
-                                var feed = component.querySelector("#files-list");
-                                var template = feed.nextElementSibling;
-                                feed.innerHTML = "";
-                                var split = uri.split('/');
-                                var urt = split.splice(5, split.length - 1);
-                                feed.path = 0 < 1 ? "" : (urt.length > 1 ? "/" : "") + urt.join("/");
-                                filetrees.path = 0 < 1 ? "" : (urt.length > 1 ? "/" : "") + urt.join("/");
-
-                                var href = uri.split("/").splice(1).filter(n=>n.length > 0);
-                                var path = href.splice(4, href.length - 1)
-                                var json = await github.repos.contents({
-                                    owner: paths[0],
-                                    repo: paths[1],
-                                    path: feed.path
-                                });
-                                json.sort((i,o)=>i.type.localeCompare(o.type));
-                                0 > 1 ? console.log(261, {
-                                    json,
-                                    path
-                                }) : null;
-                                if (json.length > 0) {
-                                    var urx = feed.path.split('/').splice(1);
-                                    //console.log(274, urx);
-                                    if (urx.length > 0) {
-                                        var ls = template.content.children[0].cloneNode(true);
-                                        var dirs = urx.length > 2 && urx.length < 5 ? [urx[0], urx[1]] : urx.splice(0, urx.length - 1);
-                                        //console.log(275, dirs, urx);
-                                        //ls.setAttribute('href', "/" + dirs.join('/'));
-                                        feed.insertAdjacentHTML('beforeend', ls.outerHTML);
-                                        feed.lastElementChild.querySelector('span').onclick = ()=>editor.tree.ls("/" + dirs.join('/'));
-                                    }
-
-                                    var folder = template.content.children[1].cloneNode(true);
-                                    //folder.querySelector('span').setAttribute('href', "/" + dirs.join('/'));
-                                    folder.querySelector('span').textContent = paths[1];
-                                    folder.setAttribute('path', '');
-                                    folder.removeAttribute('data-before');
-                                    //folder.setAttribute('sha', row.sha);
-                                    feed.insertAdjacentHTML('beforeend', folder.outerHTML);
-                                    feed.lastElementChild.querySelector('span').onclick = (e)=>editor.tree.cd(e.target.closest('text').querySelector('span').textContent);
-                                    feed.lastElementChild.classList.add('active');
-
-                                    var i = 0;
-                                    do {
-                                        var row = json[i];
-                                        //console.log(283, i, feed, row.type);
-                                        if (row.type === "dir") {
-                                            var folder = template.content.children[1].cloneNode(true);
-                                            var dirs = paths.length > 4 ? paths.concat([row.name]) : [paths[0], paths[1]].concat(["wide", "main", row.name]);
-                                            //folder.querySelector('span').setAttribute('href', "/" + dirs.join('/'));
-                                            folder.querySelector('span').textContent = row.name;
-                                            folder.setAttribute('path', row.path);
-                                            folder.setAttribute('sha', row.sha);
-                                            feed.insertAdjacentHTML('beforeend', folder.outerHTML);
-                                            feed.lastElementChild.querySelector('span').onclick = (e)=>editor.tree.cd(e.target.closest('text').querySelector('span').textContent);
-                                            feed.lastElementChild.classList.add('active');
-                                            //console.log(290, feed, folder.outerHTML);
-                                        }
-                                        if (row.type === "file") {
-                                            var file = template.content.children[2].cloneNode(true);
-                                            var dirs = paths.length > 4 ? paths.concat([row.name]) : [paths[0], paths[1]].concat(["wide", "main", row.name]);
-                                            //file.querySelector('span').setAttribute('href', "/" + dirs.join('/'));
-                                            file.querySelector('span').textContent = row.name;
-                                            file.setAttribute('path', row.path);
-                                            file.setAttribute('sha', row.sha);
-                                            feed.insertAdjacentHTML('beforeend', file.outerHTML);
-                                            feed.lastElementChild.querySelector('span').onclick = (e)=>editor.tree.nl(e.target);
-                                            Array.from(feed.children).forEach(tab=>{
-                                                tab.classList.remove('active');
-                                                tab.removeAttribute('css-display', 'none');
-                                            }
-                                            );
-                                            feed.lastElementChild.classList.add('active');
-                                            //console.log(paths[0] + '/' + paths[1], feed.path + row.name);
-                                        }
-                                        i++;
-                                    } while (i < json.length)
+                                                        
+                                    var filetrees = component.querySelector("#file-trees");
+                                    var split = uri.split('/');
+                                    var urt = split.splice(5, split.length - 1);
+                                    filetrees.path = 0 < 1 ? "" : (urt.length > 1 ? "/" : "") + urt.join("/");
                                 }
                                 var contentWindow = await wIDE(paths);
                                 //console.log(540, contentWindow);
